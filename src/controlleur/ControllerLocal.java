47b9d57b212a032b65fb12c28d5ef05badd5eec0
Florent <decosterflorent@gmail.com>
2015-11-02 11:36:40 +0100

    ControllerLocal

---
+++ src/controlleur/ControllerLocal.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlleur;
+
+import model.Coord;
+import model.Partie;
+import model.Piece;
+
+/**
+ *
+ * @author Flo
+ */
+public class ControllerLocal  implements  IControlleur{
+
+    Partie partie;
+    
+    public ControllerLocal(Partie partie) {
+        this.partie=partie ;
+    }
+
+    
+    
+    @Override
+    public boolean poserPiece(Piece piece, Coord coord) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean donnerPiece(Piece piece) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean annoncerQuarto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean annoncerMatchNul() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
