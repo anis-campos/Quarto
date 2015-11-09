3383115888feebc65f2df36a808bfbc5c69bafd2
Anis Da Silva Campos <yocorporation@hotmail.com>
2015-11-09 10:17:31 +0100

    Correction test

--- test/model/testQuartoCalculator.java
+++ test/model/testQuartoCalculator.java
@@ -8,7 +8,6 @@ package model;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import static model.QuartoCalculator.thereIsQuarto;
 import org.junit.*;
 
 /**
@@ -27,7 +26,7 @@ public class testQuartoCalculator {
 
     @Before
     public void setUp() {
-        p = new Parametre(true, true, true, true, true,true);
+        p = new Parametre(true, true, true, true, true,true,true);
         j1 = new Joueur("Joueur1", false, NumeroJoueur.J1);
         j2 = new Joueur("Joueur2", false, NumeroJoueur.J2);
         partie = new Partie(p, j1, j2);
